3b239e78e916a92c3d5762477fee937b47316e8f
Ailer-h <henriqueailer2@gmail.com>
2026-02-05 20:50:37 -0300

    Adcionei sistema rudimentar de login. Atualizei o gitignore

---
+++ backend/js/login.js
@@ -0,0 +1,34 @@
+let username_obj = document.getElementById("username");
+let password_obj = document.getElementById("password");
+
+function log_in(){
+
+    $.ajax({
+        url: "backend/php/login.php",
+        method: "post",
+        data: {
+            username: username_obj.value,
+            password: password_obj.value
+        },
+
+        success: function(data){
+            let request_result = JSON.parse(data)
+
+            console.log(request_result)
+
+            if (request_result['code'] == 200){
+                window.location.href = "frontend/html/dashboard.html"
+
+            }else if (request_result['code'] == 404){
+                console.log("User not found")
+            
+            }else if (request_result['code'] == 403){
+                console.log("Inactive")
+            
+            }else if (request_result['code'] == 401){
+                console.log("Wrong password")
+
+            }
+        }
+    })
+}

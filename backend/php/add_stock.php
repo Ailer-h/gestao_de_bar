7ff0199f23ee5a1dbaffcb113c444e20f63fe3c3
Ailer-h <henriqueailer2@gmail.com>
2026-02-17 18:20:24 -0300

    Adicionado o sistema para criar entradas no estoque e adicionada checagem para inserção de item no estoque.

--- backend/php/add_stock.php
+++ backend/php/add_stock.php
@@ -14,13 +14,31 @@
     
     }
 
-    mysqli_query($connection, "insert into estoque (nome, descricao, min_estoque, unidade_medida, id_fornecedor, valor_unitario) values ('$nome', '$desc', '$min_estoque', '$u_medida', '$id_fornec', '$valor_unitario');");
+    $fornec_query = mysqli_query($connection, "select id, acc_status from fornecedores where id = $id_fornec;");
+    $fornec_data = mysqli_fetch_assoc($fornec_query);
 
-    //Checar possibilidade de falha
-    echo '{
-        "code": 200,
-        "msg": "Login successful"
-    }'; //200 OK
+    if(empty($fornec_data)){
+        echo '{
+            "code": 404,
+            "msg": "Supplier not found"
+        }';
+    
+    }else if($fornec_data['acc_status'] != "active"){
+        echo '{
+            "code": 407,
+            "msg": "Inactive supplier"
+        }';
+    
+    }else{
+
+        mysqli_query($connection, "insert into estoque (nome, descricao, min_estoque, unidade_medida, id_fornecedor, valor_unitario) values ('$nome', '$desc', '$min_estoque', '$u_medida', '$id_fornec', '$valor_unitario');");
+
+        echo '{
+            "code": 200,
+            "msg": "Login successful"
+        }'; //200 OK
+
+    }
 
     mysqli_close($connection);
 
---
+++ backend/php/add_stock_entry.php
@@ -0,0 +1,44 @@
+<?php
+
+    include "utilities/mysql_connect.php";
+
+    $QTD_MINIMA = 10;
+
+    $id_prod = $_POST['id_prod'];
+    $qtd_produto = $_POST['qtd'];
+    $valor_compra = $_POST['valor_compra'];
+
+    if($qtd_produto <= 0){
+        $qtd_produto = $QTD_MINIMA;
+    }
+
+    $prod_query = mysqli_query($connection,"select id, status from estoque where id = $id;");
+    $prod_data = mysqli_fetch_assoc($prod_query);
+
+    if(empty($prod_data)) {
+        echo '{
+            "code": 404,
+            "msg": "Product not found"
+        }';
+    
+    }else if ($prod_data['status'] != "active"){
+        echo '{
+            "code": 407,
+            "msg": "Inactive product"
+        }';
+
+    }else{
+
+        mysqli_query($connection, "insert into entradas_estoque (id_produto, qtd, preco) values ($id_prod, $qtd_produto, $valor_compra);");
+        mysqli_query($connection, "update estoque set qtd_em_estoque = qtd_em_estoque + 1 where id = $id_prod;");
+
+        echo '{
+            "code": 200,
+            "msg": "Entry added."
+        }';
+
+    }
+
+    mysqli_close($connection);
+
+?>

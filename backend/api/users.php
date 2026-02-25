ccfc3ce09de019b5b08f1f1c16818540cbe7073e
Ailer-h <henriqueailer2@gmail.com>
2026-02-25 18:31:48 -0300

    Movi os metodos de manipulação de user para o endpoint user.php.

--- backend/api/users.php
+++ backend/api/users.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once __DIR__ . '/../../../mysql_connect.php';
+
 header("Content-type: application/json");
 
 include "user_methods/add_user.php";
@@ -37,5 +39,42 @@ if (!in_array($_SERVER['REQUEST_METHOD'], $SUPPORTED_METHODS)){
     ];
     
     echo json_encode($response);
+ 
+    function add_user($username, $password, $acc_type){
+
+        $db = new DatabaseConnection();
+        
+        $db -> connect_to_db();
+
+        $response_json = [];
+
+        $user = $db -> use_query("select user_id, username, user_password, account_type, account_status from users where username = '$username'");
+
+        if (!empty($user)){
+            $response_json = [
+                "code" => 403,
+                "code_type" => "forbidden",
+                "msg" => "User already exists"
+            ];
     
+        }else{
+
+            //TO-DO: Adicionar criptografia para a senha. 
+
+            $db -> use_query("insert into users (username, user_password, account_type) values ('$username','$password','$acc_type')");
+
+            $response_json = [
+                "code" => 200,
+                "code_type" => "created",
+                "msg" => "User added"
+            ];
+
+        }
+
+        $db -> end_connection();
+
+        return json_encode($response_json);
+
+    }
+
 }
